c784bc89fc36fc501cfe7c45f33b6de446a7cca8
Dragon-Seeker <50533513+Dragon-Seeker@users.noreply.github.com>
2021-10-08 00:22:54 -0500

    Converting Mixin stuff

--- src/main/java/twilightforest/mixin/LevelMixinPatch.java
+++ src/main/java/twilightforest/mixin/LevelMixinPatch.java
@@ -1,9 +1,41 @@
 package twilightforest.mixin;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 
 import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import twilightforest.ASMHooks;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 @Mixin(Level.class)
 public class LevelMixinPatch {
+
+    @Unique
+    private Entity entityCached;
+    @Unique
+    private AABB areaCached;
+    @Unique
+    private Predicate<? super Entity> predicateCached;
+
+    @Inject(method = "getEntities(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;", at=@At("HEAD"))
+    public void multiPartGetEntities(Entity entity, AABB area, Predicate<? super Entity> predicate, CallbackInfoReturnable<List<Entity>> cir){
+        entityCached = entity;
+        areaCached = area;
+        predicateCached = predicate;
+        //(TargetClass) (Object) this)
+    }
+
+    @ModifyVariable(method = "getEntities(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;", at=@At("RETURN"))
+    public List<Entity> multipartHitbox(List<Entity> list){
+        return ASMHooks.multipartHitbox(list, ((Level) (Object) this), entityCached, areaCached, predicateCached);
+    }
+
 }
--- src/main/java/twilightforest/mixin/ServerLevelMixinPatch.java
+++ src/main/java/twilightforest/mixin/ServerLevelMixinPatch.java
@@ -1,11 +1,36 @@
 package twilightforest.mixin;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 
 import net.minecraft.server.level.ServerLevel;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import twilightforest.ASMHooks;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 @Mixin(ServerLevel.class)
+@Debug(export = true)
 public class ServerLevelMixinPatch {
 
+    @Unique
+    int cachedID;
 
+    @Inject(method = "getEntityOrPart", at=@At(value = "HEAD", shift = At.Shift.BY, ordinal = 2))
+    public void multiPartGetEntityOrPart(int i, CallbackInfoReturnable<Entity> cir){
+        cachedID = i;
+    }
+
+    @ModifyVariable(method = "getEntityOrPart", at=@At("RETURN"))
+    public Entity multipartHitbox(@Nullable Entity o){
+        Entity entity = ((ServerLevel) (Object) this).getEntities().get(cachedID);
+        return ASMHooks.multipartFromID(o, cachedID);
+    }
 }
