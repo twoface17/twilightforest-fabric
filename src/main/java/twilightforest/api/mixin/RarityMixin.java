4c29b7b15ed64841363f9fd5606686590e80b18a
Dragon-Seeker <50533513+Dragon-Seeker@users.noreply.github.com>
2021-11-04 23:48:59 -0500

    Fix for dimensional doors

--- src/main/java/twilightforest/api/mixin/RarityMixin.java
+++ src/main/java/twilightforest/api/mixin/RarityMixin.java
@@ -14,25 +14,25 @@ import net.minecraft.world.item.Rarity;
 
 @Mixin(Rarity.class)
 public abstract class RarityMixin {
-    @SuppressWarnings("ShadowTarget")
-    @Shadow @Final @Mutable
-    private static Rarity[] $VALUES;
-
-    @SuppressWarnings("InvokerTarget")
-    @Invoker("<init>")
-    public static Rarity init(String name, int id, ChatFormatting chatFormatting) {
-        throw new AssertionError();
-    }
-
-    // add new property from the static constructor
-    // static blocks are merged into the target class (at the end)
-    static {
-        ArrayList<Rarity> values =  new ArrayList<>(Arrays.asList($VALUES));
-        Rarity last = values.get(values.size() - 1);
-
-        // add new value
-        values.add(init("TWILIGHT", last.ordinal() + 1, ChatFormatting.DARK_GREEN));
-
-        $VALUES = values.toArray(new Rarity[0]);
-    }
+//    @SuppressWarnings("ShadowTarget")
+//    @Shadow @Final @Mutable
+//    private static Rarity[] $VALUES;
+//
+//    @SuppressWarnings("InvokerTarget")
+//    @Invoker("<init>")
+//    public static Rarity init(String name, int id, ChatFormatting chatFormatting) {
+//        throw new AssertionError();
+//    }
+//
+//    // add new property from the static constructor
+//    // static blocks are merged into the target class (at the end)
+//    static {
+//        ArrayList<Rarity> values =  new ArrayList<>(Arrays.asList($VALUES));
+//        Rarity last = values.get(values.size() - 1);
+//
+//        // add new value
+//        values.add(init("TWILIGHT", last.ordinal() + 1, ChatFormatting.DARK_GREEN));
+//
+//        $VALUES = values.toArray(new Rarity[0]);
+//    }
 }
